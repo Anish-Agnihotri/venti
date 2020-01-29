18bec9f5064fdc03b83ec31ff5f9930f20fd38ff
Anish Agnihotri <contact@anishagnihotri.com>
2020-01-28 23:40:11 -0500

    Opensign default + code abstraction

--- ide/src/stores/files.js
+++ ide/src/stores/files.js
@@ -1,12 +1,13 @@
 // Import createConnectedStore to create undux store.
 import { createConnectedStore } from 'undux';
+import { opensign_test_code } from './sample';
 
 // TODO: Setup persistance 
 
 // Create files store
 export default createConnectedStore({
 	// Files stores all existing file names, code, shown switch, and col/row number of cursor
-	files: [{"name": "sign_test.sol", "code": "pragma solidity 0.5.0;", "shown": true}],
+	files: [{"name": "opensign_test.sol", "code": opensign_test_code, "shown": true, "row": 27, "col": 1}],
 	// Tab Management is used for global editor tab management.
 	tabMgmt: [0],
 })
